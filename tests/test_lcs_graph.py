5cbdd77ccf899e2c5f3142acfb835a2492e2b785
nntto <nattou-king@hotmail.co.jp>
2023-10-11 13:56:04 +0900

    LCSグラフのテストコードから，LCSのDPを行う関数を分離

--- tests/test_lcs_graph.py
+++ tests/test_lcs_graph.py
@@ -1,19 +1,16 @@
-import json
 import unittest
-import numpy as np
-from numpy.testing import assert_array_equal
 
-from lcs import LCS
 from lcs_graph import LCSGraph
+from my_enum import Direction
 
 
 class TestLCSGraph(unittest.TestCase):
     def test_empty_strings(self):
         S = ""
         T = ""
-        lcs = LCS(S, T)
+        previous_position_dict = {}
 
-        lcs_graph = LCSGraph(lcs.previous_position_dict, S, T)
+        lcs_graph = LCSGraph(previous_position_dict, S, T)
         self.assertEqual([(0, 0)], lcs_graph.V_G)
         self.assertEqual({(0, 0): []}, lcs_graph.E_G)
         self.assertEqual({}, lcs_graph.edge_label)
@@ -22,9 +19,56 @@ class TestLCSGraph(unittest.TestCase):
         self.maxDiff = None  # diff出力の文字数制限を解除
         S = "ABACB"
         T = "BABBCAB"
-        lcs = LCS(S, T)
-
-        lcs_graph = LCSGraph(lcs.previous_position_dict, S, T)
+        previous_position_dict = {
+            (1, 0): [Direction.UP],
+            (2, 0): [Direction.UP],
+            (3, 0): [Direction.UP],
+            (4, 0): [Direction.UP],
+            (5, 0): [Direction.UP],
+            (0, 1): [Direction.LEFT],
+            (0, 2): [Direction.LEFT],
+            (0, 3): [Direction.LEFT],
+            (0, 4): [Direction.LEFT],
+            (0, 5): [Direction.LEFT],
+            (0, 6): [Direction.LEFT],
+            (0, 7): [Direction.LEFT],
+            (1, 1): [Direction.UP, Direction.LEFT, Direction.UPPER_LEFT],
+            (1, 2): [Direction.UPPER_LEFT],
+            (1, 3): [Direction.LEFT],
+            (1, 4): [Direction.LEFT],
+            (1, 5): [Direction.LEFT],
+            (1, 6): [Direction.LEFT, Direction.UPPER_LEFT],
+            (1, 7): [Direction.LEFT],
+            (2, 1): [Direction.UPPER_LEFT],
+            (2, 2): [Direction.UP, Direction.LEFT],
+            (2, 3): [Direction.UPPER_LEFT],
+            (2, 4): [Direction.LEFT, Direction.UPPER_LEFT],
+            (2, 5): [Direction.LEFT],
+            (2, 6): [Direction.LEFT],
+            (2, 7): [Direction.LEFT, Direction.UPPER_LEFT],
+            (3, 1): [Direction.UP],
+            (3, 2): [Direction.UPPER_LEFT],
+            (3, 3): [Direction.UP, Direction.LEFT],
+            (3, 4): [Direction.UP, Direction.LEFT, Direction.UPPER_LEFT],
+            (3, 5): [Direction.UP, Direction.LEFT, Direction.UPPER_LEFT],
+            (3, 6): [Direction.UPPER_LEFT],
+            (3, 7): [Direction.LEFT],
+            (4, 1): [Direction.UP],
+            (4, 2): [Direction.UP],
+            (4, 3): [Direction.UP, Direction.LEFT, Direction.UPPER_LEFT],
+            (4, 4): [Direction.UP, Direction.LEFT, Direction.UPPER_LEFT],
+            (4, 5): [Direction.UPPER_LEFT],
+            (4, 6): [Direction.UP, Direction.LEFT],
+            (4, 7): [Direction.UP, Direction.LEFT, Direction.UPPER_LEFT],
+            (5, 1): [Direction.UP, Direction.UPPER_LEFT],
+            (5, 2): [Direction.UP],
+            (5, 3): [Direction.UPPER_LEFT],
+            (5, 4): [Direction.LEFT, Direction.UPPER_LEFT],
+            (5, 5): [Direction.UP, Direction.LEFT],
+            (5, 6): [Direction.UP, Direction.LEFT, Direction.UPPER_LEFT],
+            (5, 7): [Direction.UPPER_LEFT],
+        }
+        lcs_graph = LCSGraph(previous_position_dict, S, T)
 
         self.assertEqual(
             [
